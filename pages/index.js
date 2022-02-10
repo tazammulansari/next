6ed43191125c6d591639f72a96a1ea02128f1beb
tazammulansari <93309643+tazammulansari@users.noreply.github.com>
2022-02-10 10:48:51 +0000

    hello

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.jsx!</a>
+          Welcome to <a href="https://nextjs.org">Next</a>
         </h1>
 
         <p className={styles.description}>
